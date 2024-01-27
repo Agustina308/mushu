247e1a0e5c2923c6d83bf4ffffd2623ebb977a0f
Agustina308 <agusmansillacrp@gmail.com>
2024-01-27 18:49:53 -0300

    app.py

---
+++ Backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+import os
+
+app = Flask(__name__, template_folder='../Front/templates')
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/lista', methods=['GET', 'POST'])
+def lista():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "File has been uploaded."
+    return render_template('lista.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
